663645b46422ee28dab2015d963b7db52ac1ac65
PotatoPopcorn <potatopopcorn@potatopopcorn.com>
2019-12-13 01:42:28 +1000

    Add test for devices

---
+++ tests/testdevices.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <string>
+#include <vector>
+
+#include "../pldevices.h"
+
+int main(int argc, char **argv)
+{
+    PlanktonLighting::PLDevices devices;
+    std::vector<std::string> types = devices.getSupportedTypes();
+    printf("There is %lu types supported: \n", types.size());
+    for(int i = 0; i < types.size(); ++i)
+    {
+        printf(" + %s\n", types[i].c_str());
+    }
+
+    std::vector<std::string> ava = devices.getAvailableDevices();
+    printf("There is %lu devices avaliable: \n", ava.size());
+    for(int i = 0; i < ava.size(); ++i)
+    {
+        printf(" + %s\n", ava[i].c_str());
+    }
+    return 0;
+}
