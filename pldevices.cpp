4fbe1fa235f693f9b87a4fb104ae2168126009e4
PotatoPopcorn <potatopopcorn@potatopopcorn.com>
2019-12-12 23:08:56 +1000

    Add supported types to devices

--- pldevices.cpp
+++ pldevices.cpp
@@ -3,11 +3,15 @@
 std::vector<std::string> PlanktonLighting::PLDevices::getSupportedTypes()
 {
     std::vector<std::string> types;
+    types.push_back("dummy");
+    types.push_back("artnet");
+    types.push_back("enttecpro");
     return types;
 }
 
 std::vector<std::string> PlanktonLighting::PLDevices::getAvailableDevices()
 {
     std::vector<std::string> devices;
+    
     return devices;
 }
