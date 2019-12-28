3a48db159aa08874b082e478e3959b94e115136d
PotatoPopcorn <potatopopcorn@potatopopcorn.com>
2019-12-28 22:42:55 +1000

    Add OpenDMX class

---
+++ src/pldeviceopendmx.cpp
@@ -0,0 +1,59 @@
+//Copyright 2019 PotatoPopcorn (Tony Kerr)
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// See LICENCE
+
+#include "pldeviceopendmx.h"
+
+//Empty constructor
+PlanktonLighting::PLDeviceOpenDMX::PLDeviceOpenDMX()
+{
+
+}
+
+//Empty destructor
+PlanktonLighting::PLDeviceOpenDMX::~PLDeviceOpenDMX()
+{
+
+}
+
+bool PlanktonLighting::PLDeviceOpenDMX::initDevice(std::string args)
+{
+
+}
+
+bool PlanktonLighting::PLDeviceOpenDMX::closeDevice()
+{
+
+}
+
+bool PlanktonLighting::PLDeviceOpenDMX::sendDMX(PlanktonLighting::PLUniverse *uni)
+{
+
+}
+
+bool PlanktonLighting::PLDeviceOpenDMX::sendDMX(PlanktonLighting::PLUniverse *uni, std::string args)
+{
+    sendDMX(uni);
+}
+
+std::string PlanktonLighting::PLDeviceOpenDMX::sendMSG(std::string args)
+{
+
+}
+
+bool PlanktonLighting::PLDeviceOpenDMX::processArgs(std::string args)
+{
+
+}
---
+++ src/pldeviceopendmx.h
@@ -0,0 +1,48 @@
+//Copyright 2019 PotatoPopcorn (Tony Kerr)
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// See LICENCE
+
+#ifndef PLDEVICEOPENDMX_H
+#define PLDEVICEOPENDMX_H
+
+#include <string>
+
+#include "ftd2xx.h"
+
+#include "pldevice.h"
+#include "pluniverse.h"
+
+namespace PlanktonLighting
+{
+    class PLDeviceOpenDMX : public PLDevice{
+    public:
+        PLDeviceOpenDMX();
+        ~PLDeviceOpenDMX();
+
+        bool initDevice(std::string args);
+        bool closeDevice();
+
+        bool sendDMX(PlanktonLighting::PLUniverse *universe);
+        bool sendDMX(PlanktonLighting::PLUniverse *universe, std::string args);
+        std::string sendMSG(std::string args);
+
+    private:
+        int devNum = 0;
+        int frequency = 25;
+        bool processArgs(std::string args);
+    };
+}
+
+#endif //PLDEVICEOPENDMX_H
